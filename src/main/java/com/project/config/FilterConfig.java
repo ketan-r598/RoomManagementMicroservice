7dddd7719c851a8a9f8708778fbdac20d62b113e
Ketan R <ketan_r598@outlook.com>
2023-06-21 20:06:38 +0530

    Added Configuration for filters

---
+++ src/main/java/com/project/config/FilterConfig.java
@@ -0,0 +1,22 @@
+package com.project.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.GenericFilterBean;
+
+import com.project.filter.JWTValidationFilter;
+
+@Configuration
+public class FilterConfig {
+	public static final String PATH = "/admin/rooms/*";
+	
+	@Bean
+	public FilterRegistrationBean<GenericFilterBean> jwtFilter(){
+		FilterRegistrationBean<GenericFilterBean> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new JWTValidationFilter());
+		filterRegistrationBean.addUrlPatterns(PATH);
+		return filterRegistrationBean;
+	}
+	
+}
