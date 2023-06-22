f0459c3c58481e4a2580e420d2dbdadc27eb7f01
Ketan R <ketan_r598@outlook.com>
2023-06-22 09:40:33 +0530

    Updated code

--- src/main/java/com/project/config/FilterConfig.java
+++ src/main/java/com/project/config/FilterConfig.java
@@ -6,16 +6,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.GenericFilterBean;
 
 import com.project.filter.JWTValidationFilter;
+import com.project.session.Session;
 
 @Configuration
 public class FilterConfig {
-	public static final String PATH = "/admin/rooms/*";
+	
+	public static final String OWNERPATH = "/rooms/owner/*";
+	public static final String CUSTOMERPATH = "/rooms/customer/*";
+	
+	private Session session = Session.getSession();
 	
 	@Bean
 	public FilterRegistrationBean<GenericFilterBean> jwtFilter(){
 		FilterRegistrationBean<GenericFilterBean> filterRegistrationBean = new FilterRegistrationBean<>();
 		filterRegistrationBean.setFilter(new JWTValidationFilter());
-		filterRegistrationBean.addUrlPatterns(PATH);
+		
+		if(session.getRole().equalsIgnoreCase("customer")) {
+			filterRegistrationBean.addUrlPatterns(CUSTOMERPATH);
+		} else if(session.getRole().equalsIgnoreCase("owner")){
+			filterRegistrationBean.addUrlPatterns(OWNERPATH);
+		} else {
+			filterRegistrationBean.addUrlPatterns("");
+		}
+		
 		return filterRegistrationBean;
 	}
 	
