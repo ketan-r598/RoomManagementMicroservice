77c29170a3e57cf08a1253c093f27c03974a2b4a
rkumar891 <rkumar891@US-124K9S3>
2023-06-22 17:23:32 +0530

    add filter

--- src/main/java/com/project/filter/JWTValidationFilter.java
+++ src/main/java/com/project/filter/JWTValidationFilter.java
@@ -1,17 +1,11 @@
 package com.project.filter;
-<<<<<<< HEAD
-=======
 
->>>>>>> ab7a1c5d0bbf3da33029ff9eb708f7500a50a491
 import java.io.IOException;
 
 import org.springframework.web.filter.GenericFilterBean;
 
-<<<<<<< HEAD
-=======
 import com.project.session.Session;
 
->>>>>>> ab7a1c5d0bbf3da33029ff9eb708f7500a50a491
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
@@ -21,55 +15,14 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
-public class JWTValidationFilter extends GenericFilterBean{
-	
-=======
 public class JWTValidationFilter extends GenericFilterBean {
 
->>>>>>> ab7a1c5d0bbf3da33029ff9eb708f7500a50a491
 	public static final String BEARER = "Bearer";
 	public static final String AUTHORIZATION = "Authorization";
 
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
 			throws IOException, ServletException {
-<<<<<<< HEAD
-	
-		HttpServletRequest request = (HttpServletRequest)servletRequest;
-		HttpServletResponse response = (HttpServletResponse)servletResponse;
-		final String authorization = request.getHeader(AUTHORIZATION);
-		if(authorization == null || !authorization.startsWith(BEARER)) {
-			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			response.getWriter().println("Auth Header is missing");
-		}
-		
-		else {
-		// Bearer tokenValue
-		String token = authorization.substring(7);
-		try {
-			final Claims claims = Jwts.parser().setSigningKey("testsecretkey")
-					.parseClaimsJws(token)
-					.getBody();
-			request.setAttribute("claims", claims);
-			System.out.println(claims.getSubject());
-			//String role = (String)claims.get("roles");
-			//System.out.println(role);
-			chain.doFilter(request, response);
-			
-		}
-		catch(Exception e) {
-			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-			response.getWriter().println("Invalid Token");
-		}
-		}
-		
-	}
-	
-	
-
-}
-=======
 
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
@@ -130,4 +83,3 @@ public class JWTValidationFilter extends GenericFilterBean {
 		}
 	}
 }
->>>>>>> ab7a1c5d0bbf3da33029ff9eb708f7500a50a491
