681e44178748abe01c50b3bdac28d5c3eb77c703
Ketan R <ketan_r598@outlook.com>
2023-06-22 09:40:53 +0530

    Updated code

--- src/main/java/com/project/filter/JWTValidationFilter.java
+++ src/main/java/com/project/filter/JWTValidationFilter.java
@@ -1,7 +1,10 @@
 package com.project.filter;
+
 import java.io.IOException;
 
 import org.springframework.web.filter.GenericFilterBean;
+
+import com.project.session.Session;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -12,45 +15,59 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class JWTValidationFilter extends GenericFilterBean{
-	
+public class JWTValidationFilter extends GenericFilterBean {
+
 	public static final String BEARER = "Bearer";
 	public static final String AUTHORIZATION = "Authorization";
 
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
 			throws IOException, ServletException {
-	
-		HttpServletRequest request = (HttpServletRequest)servletRequest;
-		HttpServletResponse response = (HttpServletResponse)servletResponse;
+
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+		Session session = Session.getSession();
+		
 		final String authorization = request.getHeader(AUTHORIZATION);
-		if(authorization == null || !authorization.startsWith(BEARER)) {
+
+		if (authorization == null || !authorization.startsWith(BEARER)) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			response.getWriter().println("Auth Header is missing");
 		}
-		
-		else {
-		// Bearer tokenValue
-		String token = authorization.substring(7);
-		try {
-			final Claims claims = Jwts.parser().setSigningKey("testsecretkey")
-					.parseClaimsJws(token)
-					.getBody();
-			request.setAttribute("claims", claims);
-			System.out.println(claims.getSubject());
-			//String role = (String)claims.get("roles");
-			//System.out.println(role);
-			chain.doFilter(request, response);
-			
-		}
-		catch(Exception e) {
-			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-			response.getWriter().println("Invalid Token");
-		}
-		}
-		
-	}
-	
-	
 
+		else {
+			// Bearer tokenValue
+			String token = authorization.substring(7);
+
+			try {
+
+				final Claims claims = Jwts
+									.parser()
+									.setSigningKey("testsecretkey")
+									.parseClaimsJws(token)
+									.getBody();
+
+				request.setAttribute("claims", claims);
+
+				System.out.println(claims.getSubject());
+				
+				int userId = (int) claims.get("userId");
+				String email = (String) claims.getSubject();
+				String password = (String) claims.get("password");
+				String role = (String) claims.get("role");
+				
+				session.setUserId(userId);
+				session.setEmail(email);
+				session.setPassword(password);
+				session.setRole(role);
+		
+				chain.doFilter(request, response);
+			
+			} catch (Exception e) {
+				response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+				response.getWriter().println("Invalid Token");
+			}
+		}
+	}
 }
