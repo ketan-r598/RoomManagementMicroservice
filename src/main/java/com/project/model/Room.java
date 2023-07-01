4e2dcdb7205a65807ec975cf5aab2488171c7cd8
Rakshit Kumar <mail.me.at.rakshith@gmail.com>
2023-07-01 18:45:08 +0530

    added generatedId and changed db to hms

--- src/main/java/com/project/model/Room.java
+++ src/main/java/com/project/model/Room.java
@@ -1,6 +1,8 @@
 package com.project.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -9,6 +11,7 @@ import jakarta.persistence.Table;
 public class Room {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int roomId;
 	
 	private int hotelId;
